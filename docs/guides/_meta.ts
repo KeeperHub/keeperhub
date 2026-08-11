f0d7a5fc96ea443c7abe1089ce566bc1481ce679
bit-nexus-labs <289002433+bit-nexus-labs@users.noreply.github.com>
2026-08-11 03:15:01 +0300

    Re-add 'language-helpers' entry in _meta.ts

--- docs/guides/_meta.ts
+++ docs/guides/_meta.ts
@@ -1,7 +1,7 @@
 export default {
-  "language-helpers": "Language-specific Helpers",
   index: "Overview",
   "first-verified-transaction": "Zero to a Verified Onchain Transaction",
   "defender-migration": "Migrate from OpenZeppelin Defender",
   "gelato-migration": "Migrate from Gelato Functions",
+  "language-helpers": "Language-specific Helpers",
 };
